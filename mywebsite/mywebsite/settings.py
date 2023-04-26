10a3ef3dd103db9dd5f4763719568b3ef9e24a66
TinSaCrab <125628895+TinSaCrab@users.noreply.github.com>
2023-04-26 11:58:51 +0700

    เพิ่ม BASE_DIR สำหรับ template directory

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp/template' ],
+        # BASE_DIR / 'myapp/template' C:\Users\JPTL04302019\Django Marathon\mywebsite\myapp\template
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
